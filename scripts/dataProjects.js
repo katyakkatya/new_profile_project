94475e86f2263768bd8d7255d015542a5fd603de
Katya <klenovakatusha@gmail.com>
2025-11-05 19:12:57 +0300

    kr3

--- scripts/dataProjects.js
+++ scripts/dataProjects.js
@@ -2,21 +2,25 @@ const projectData = {
     todolist: {
         description: `
         <p>Было разработано приложение для списка дел на языке JavaScript</p>
-        <p>GitHab: katyakkatya</p>`
+        <p>GitHab: katyakkatya</p>`,
+        image: "../images/project1-900.png"
     },
     website: {
         description: `
         <p>Был разработан личный сайт на HTML/CSS</p>
-        <p>GitHab: katyakkatya</p>`
+        <p>GitHab: katyakkatya</p>`,
+        image: "../images/project2-900.png"
     },
     portfolio: {
         description: `
         <p>Был разработан сайт-портфолио на Bootstrap</p>
-        <p>GitHab: katyakkatya</p>`
+        <p>GitHab: katyakkatya</p>`,
+        image: "../images/project3-900.png"
     },
     store: {
         description: `
         <p>Был разработан интернет-иагазин на React</p>
-        <p>GitHab: katyakkatya</p>`
+        <p>GitHab: katyakkatya</p>`,
+        image: "../images/project4-900.png"
     },
 }
--- scripts/projectModal.js
+++ scripts/projectModal.js
@@ -3,6 +3,7 @@ class ProjectModal {
         this.modal = document.getElementById('modal');
         this.backdrop = document.querySelector('.modal-backdrop');
         this.description = document.querySelector('.project-modal__description-content');
+        this.image = document.querySelector('.project-modal__img');
         this.init();
     }
 
@@ -12,6 +13,12 @@ class ProjectModal {
                 this.close();
             }
         })
+
+        document.addEventListener('keydown', (e) => {
+            if (e.key === 'Escape') {
+                this.close();
+            }
+        });
     }
 
     open(projectData) {
@@ -30,6 +37,7 @@ class ProjectModal {
 
     fillContent(projectData) {
         this.description.innerHTML = projectData.description;
+        this.image.src = projectData.image;
     }
 }
 
@@ -39,13 +47,36 @@ document.addEventListener('DOMContentLoaded', function () {
     projectModal = new ProjectModal;
 });
 
+
+modal.addEventListener('keydown', function(event) {
+ if (event.key === 'Tab') {
+    const focusableElements = modal.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+    const firstElement = focusableElements[0];
+    const lastElement = focusableElements[focusableElements.length - 1];
+
+    // Зацикливаем фокус внутри модалки
+    if (event.shiftKey && document.activeElement === firstElement) {
+        event.preventDefault();
+        lastElement.focus();
+    } else if (!event.shiftKey && document.activeElement === lastElement) {
+        event.preventDefault();
+        firstElement.focus();
+    }
+ }
+ });
+
+
 function openModal(projectId) {
     const project = projectData[projectId];
+    previousActiveElement = document.activeElement;
     if (project) {
         projectModal.open(project);
+        modal.focus();
+        document.querySelectorAll('body > *:not(.modal):not(.overlay)').forEach(el => el.setAttribute('aria-hidden', 'true'));
     }
 }
 
 function closeModal() {
     projectModal.close();
+    if (previousActiveElement) { previousActiveElement.focus(); }
 }
